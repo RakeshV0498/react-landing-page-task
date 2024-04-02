c8cffd818c49e7985e1cbbe79fab507d3ce83908
RakeshV0498 <rockr4674@gmail.com>
2024-04-02 19:28:14 +0530

    Updated Features Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/hero";
+import { features } from "./data";
+import Features from "./components/Features/Features";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <section className="features-icons bg-light text-center">
+        <section className="container">
+          <div className="row">
+            {features.map((feature) => (
+              <Features
+                key={feature.id}
+                icon={feature.icon}
+                title={feature.title}
+                desc={feature.desc}
+              />
+            ))}
+          </div>
+        </section>
+      </section>
     </>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,16 +1,18 @@
 const features = [
   {
+    id: 1,
     icon: "bi-window",
-
     title: "Fully Responsive",
     desc: "This theme will look great on any device, no matter the size!",
   },
   {
+    id: 2,
     icon: "bi-layers",
     title: "Bootstrap 5 Ready",
     desc: "Featuring the latest build of the new Bootstrap 5 framework!",
   },
   {
+    id: 3,
     icon: "bi-terminal",
     title: "Easy to Use",
     desc: "Ready to use with your own content, or customize the source files!",
