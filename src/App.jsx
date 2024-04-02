938398d2adf674b3403ddf2ad8f22cbaee12b22e
RakeshV0498 <rockr4674@gmail.com>
2024-04-02 16:43:53 +0530

    Updated Navbar component and Navbar CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar/Navbr";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
