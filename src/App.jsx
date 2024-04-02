cfc67e5e5d4ead6d06499404eb5396a91e68acf8
RakeshV0498 <rockr4674@gmail.com>
2024-04-02 18:04:19 +0530

    Removed Unnecessary Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/hero";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
