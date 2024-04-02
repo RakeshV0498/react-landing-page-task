c2593e756fa2f4eaf59dba56482fcc6fb166ecac
RakeshV0498 <rockr4674@gmail.com>
2024-04-02 18:15:32 +0530

    Updated Data for features

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const features = [
+  {
+    icon: "bi-window",
+
+    title: "Fully Responsive",
+    desc: "This theme will look great on any device, no matter the size!",
+  },
+  {
+    icon: "bi-layers",
+    title: "Bootstrap 5 Ready",
+    desc: "Featuring the latest build of the new Bootstrap 5 framework!",
+  },
+  {
+    icon: "bi-terminal",
+    title: "Easy to Use",
+    desc: "Ready to use with your own content, or customize the source files!",
+  },
+];
+
+export { features };
